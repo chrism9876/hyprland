55996522cc6378e75cfc9549e536238a0ac9ee9d
chrism9876 <chrism9876@hotmail.co.uk>
2024-05-31 23:54:38 +0100

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -6,7 +6,7 @@ sudo pacman -S --needed polkit-kde-agent xdg-desktop-portal-hyprland
 
 sudo pacman -S --needed breeze-icons ttf-cascadia-code-nerd
 
-sudo pacman -S --needed pavucontrol kitty dolphin
+sudo pacman -S --needed pavucontrol kitty dolphin brightnessctl blueman
 
 sudo pacman -S --needed hyprland hyprlock hyprpaper waybar wofi
 
