178aa6bbb682d3acb880298188f0d5d9f37d94c3
chrism9876 <chrism9876@hotmail.co.uk>
2024-06-13 13:51:47 +0100

    add flameshot deps

--- hyprland.sh
+++ hyprland.sh
@@ -6,7 +6,7 @@ sudo pacman -S --needed polkit-kde-agent xdg-desktop-portal-hyprland
 
 sudo pacman -S --needed breeze-icons ttf-cascadia-code-nerd
 
-sudo pacman -S --needed pavucontrol kitty imv dolphin brightnessctl blueman bluez bluez-utils grim slurp
+sudo pacman -S --needed pavucontrol kitty imv dolphin brightnessctl blueman bluez bluez-utils grim slurp flameshot
 
 sudo pacman -S --needed hyprland hyprlock hyprpaper waybar wofi
 
