a9d78c57ee429bcdbe0da5bb81e4ed0729fad2ff
chrism9876 <chrism9876@hotmail.co.uk>
2024-06-21 08:13:31 +0100

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -6,8 +6,10 @@ sudo pacman -S --needed polkit-kde-agent xdg-desktop-portal-hyprland
 
 sudo pacman -S --needed breeze-icons ttf-cascadia-code-nerd
 
-sudo pacman -S --needed pavucontrol kitty imv dolphin brightnessctl blueman bluez bluez-utils grim slurp flameshot-git
+sudo pacman -S --needed pavucontrol kitty imv dolphin brightnessctl blueman bluez bluez-utils grim slurp
 
 sudo pacman -S --needed hyprland hyprlock hyprpaper waybar wofi
+
+echo "To install flameshot run: yay -S flameshot-git"
 
 cp -r hypr/ ~/.config/
